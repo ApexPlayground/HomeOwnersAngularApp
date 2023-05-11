83a031b93fa3f0b05f80c7056b9e00a8c09dec5e
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-05-11 13:53:43 +0100

    Data update

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,56 +2,61 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  questions = [{
-    text: 'Best places to visit in Newcastle?',
-    id: 1,
-    userId: 1
-  }, {
-    text: 'Best food to eat in Mumbai?',
-    id: 2,
-    userId: 2
-  }];
+  questions = [
+    {
+      text: 'How to ride a bike?',
+      id: 1,
+      userId: 1,
+    },
+    {
+      text: 'How to play a guiter?',
+      id: 2,
+      userId: 2,
+    },
+  ];
 
-  answers = [{
-    id: 1,
-    questionId: 1,
-    text: 'City Center, River Tyne',
-    userId: 3
-  },
-  {
-    id: 1,
-    questionId: 1,
-    text: 'Gateshead, Quayside',
-    userId: 2
-  },
-  {
-    id: 1,
-    questionId: 2,
-    text: 'Vadapaav, Pani puri, Paav bhaji, Misal etc',
-    userId: 3
-  }];
+  answers = [
+    {
+      id: 1,
+      questionId: 1,
+      text: "To ride a bike, start by choosing a bike that fits you well. Find a safe, open area to practice, and put on a helmet for safety. Begin by straddling the bike and getting a feel for balance by lifting your feet off the ground. When you're ready, push off with one foot and start pedaling. Use your brakes to control your speed and gently lean and turn the handlebars to steer. Practice regularly to improve your skills and gradually increase your confidence. Enjoy the freedom and fun that riding a bike brings!",
+      userId: 3,
+    },
+    {
+      id: 1,
+      questionId: 1,
+      text: 'Put your leg on the pedal and start riding',
+      userId: 2,
+    },
+    {
+      id: 1,
+      questionId: 2,
+      text: "To play the guitar, follow these simple steps. First, hold the guitar correctly by placing your dominant hand on the neck and your other hand on the body. Learn the basic chords, such as E, A, D, G, and C, which are essential for many songs. Practice transitioning between chords smoothly and accurately. Next, work on your strumming technique by using a pick or your fingers to strike the strings in a rhythmic pattern. Start with simple strumming patterns and gradually increase the complexity as you progress. Additionally, learn to read guitar tablature or sheet music to play melodies and solos. Practice regularly to build strength, dexterity, and muscle memory. Utilize online tutorials, instructional books, or seek guidance from a guitar teacher to enhance your learning experience. Finally, enjoy the process and have fun exploring the endless possibilities of creating music with your guitar.",
+      userId: 3,
+    },
+  ];
 
   constructor(private authService: AuthService) { }
 
   getQuestionById(id: any) {
-    const question: any = this.questions.find(question => question.id === id);
+    const question: any = this.questions.find((question) => question.id === id);
     const user: any = this.authService.getUser(question.userId);
     return {
       ...question,
       userName: user.name,
-      userId: user.id
-    }
+      userId: user.id,
+    };
   }
 
   getQuestions() {
     return this.questions.map((question: any) => {
       return {
         ...question,
-        user: this.authService.getUser(question.userId)
-      }
+        user: this.authService.getUser(question.userId),
+      };
     });
   }
 
@@ -67,7 +72,7 @@ export class DataService {
       id,
       questionId,
       text: answer,
-      userId
+      userId,
     });
     return true;
   }
@@ -78,12 +83,12 @@ export class DataService {
     }
 
     return this.answers
-      .filter(answer => answer.questionId === questionId)
+      .filter((answer) => answer.questionId === questionId)
       .map((answer: any) => {
         return {
           ...answer,
-          user: this.authService.getUser(answer.userId)
-        }
+          user: this.authService.getUser(answer.userId),
+        };
       });
   }
 }
