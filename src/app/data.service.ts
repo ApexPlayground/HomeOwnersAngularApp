1ced4fc8fbfb5f229e7518f73aaff85df19407f0
Lou-John <loujohn.genthon@gmail.com>
2023-05-11 14:16:54 +0100

    upvote answer

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -24,18 +24,21 @@ export class DataService {
       questionId: 1,
       text: "To ride a bike, start by choosing a bike that fits you well. Find a safe, open area to practice, and put on a helmet for safety. Begin by straddling the bike and getting a feel for balance by lifting your feet off the ground. When you're ready, push off with one foot and start pedaling. Use your brakes to control your speed and gently lean and turn the handlebars to steer. Practice regularly to improve your skills and gradually increase your confidence. Enjoy the freedom and fun that riding a bike brings!",
       userId: 3,
+      upvote: 99,
     },
     {
       id: 1,
       questionId: 1,
       text: 'Put your leg on the pedal and start riding',
       userId: 2,
+      upvote: 2,
     },
     {
       id: 1,
       questionId: 2,
       text: "To play the guitar, follow these simple steps. First, hold the guitar correctly by placing your dominant hand on the neck and your other hand on the body. Learn the basic chords, such as E, A, D, G, and C, which are essential for many songs. Practice transitioning between chords smoothly and accurately. Next, work on your strumming technique by using a pick or your fingers to strike the strings in a rhythmic pattern. Start with simple strumming patterns and gradually increase the complexity as you progress. Additionally, learn to read guitar tablature or sheet music to play melodies and solos. Practice regularly to build strength, dexterity, and muscle memory. Utilize online tutorials, instructional books, or seek guidance from a guitar teacher to enhance your learning experience. Finally, enjoy the process and have fun exploring the endless possibilities of creating music with your guitar.",
       userId: 3,
+      upvote: 0,
     },
   ];
 
@@ -68,11 +71,13 @@ export class DataService {
 
   addAnswer(questionId: number, answer: string, userId: number) {
     const id = this.answers.length + 1;
+    const upvote = 0;
     this.answers.push({
       id,
       questionId,
       text: answer,
       userId,
+      upvote,
     });
     return true;
   }
