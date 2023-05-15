f4013cbf68ac6577616f749260dbdfdec705b5b5
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-05-15 16:05:07 +0100

    added comments to explain code

--- src/assets/search-function.ts
+++ src/assets/search-function.ts
@@ -1,3 +1,9 @@
+/**
+ * Filters an array of items based on a search term.
+ * @param items An array of strings to be filtered.
+ * @param searchTerm The search term to match against the items.
+ * @returns An array of filtered strings that include the search term.
+ */
 export function filterItems(items: string[], searchTerm: string): string[] {
   return items.filter((item: string) =>
     item.toUpperCase().includes(searchTerm.toUpperCase())
