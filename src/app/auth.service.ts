19a1b31888eb6771709616401d6ba5916f10f06f
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-05-15 15:45:18 +0100

    Added comments to explain code

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   _loggedInUser: any;
   userKey: string = 'User';
+
+  //essential users
   users: any = [
     {
       id: 1,
@@ -13,7 +15,7 @@ export class AuthService {
       email: 'j@j.com',
       password: 'root',
       expert: true,
-      isAdminUser: false
+      isAdminUser: false,
     },
     {
       id: 2,
@@ -21,7 +23,7 @@ export class AuthService {
       email: 't@t.com',
       password: 'test',
       expert: false,
-      isAdminUser: false
+      isAdminUser: false,
     },
     {
       id: 3,
@@ -29,7 +31,7 @@ export class AuthService {
       email: 's@s.com',
       password: 'Test123',
       expert: false,
-      isAdminUser: false
+      isAdminUser: false,
     },
     {
       id: 4,
@@ -37,36 +39,43 @@ export class AuthService {
       email: 'a@a.com',
       password: 'admin',
       isAdminUser: true,
-      expert: true
-    }
+      expert: true,
+    },
   ];
 
+  // Getter for the currently logged in user
   get currentLoggedInUser(): any {
     return this._loggedInUser;
   }
 
+  // Getter for checking if the currently logged in user is an expert
   get currentLoggedInExpert(): any {
     return this._loggedInUser ? this._loggedInUser.expert : false;
   }
 
+  // Getter for checking if the currently logged in user is an admin user
   get currentLoggedInAdmin(): any {
     return this._loggedInUser ? this._loggedInUser.isAdminUser : false;
   }
 
+  // Setter for setting the currently logged in user
   set currentLoggedInUser(value: any) {
     this._loggedInUser = value;
   }
 
   constructor() { }
 
+  // Get a specific user by their ID
   getUser(userId: number) {
     return this.users.find((user: any) => user.id === userId);
   }
 
+  // Check if a user ID is equal to the ID of the currently logged in user
   getUserEquals(userId: number) {
     return this.currentLoggedInUser && this.currentLoggedInUser.id === userId;
   }
 
+  // Add a new user to the users array
   addUser(name: string, email: string, password: string) {
     const newUserId = this.users.length + 1;
     this.users.push({
@@ -74,11 +83,12 @@ export class AuthService {
       name,
       email,
       password,
-      expert: false
+      expert: false,
     });
     return true;
   }
 
+  // Perform user login
   login(email: string, password: string) {
     const user = this.users.find(
       (user: any) => user.email === email && user.password === password
@@ -88,6 +98,7 @@ export class AuthService {
     return user;
   }
 
+  // Perform user logout
   logout() {
     this.currentLoggedInUser = undefined;
     localStorage.removeItem(this.userKey);
